6cffa329ee0ff929716dda83fbcbdc9677d06c7a
Ryan Kim <iawal86@gmail.com>
2020-09-08 09:24:39 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0U1YMHB1T5jYDUQzth4dzIfL6QQE01qg",
+  authDomain: "fb-clone-9417f.firebaseapp.com",
+  databaseURL: "https://fb-clone-9417f.firebaseio.com",
+  projectId: "fb-clone-9417f",
+  storageBucket: "fb-clone-9417f.appspot.com",
+  messagingSenderId: "217926234227",
+  appId: "1:217926234227:web:7bfe3354987748cb8c3a06",
+};
